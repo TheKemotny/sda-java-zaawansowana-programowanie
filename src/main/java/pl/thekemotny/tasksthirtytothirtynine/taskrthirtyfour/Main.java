2f1e234152d824da52944ae95adfae03b99a5e88
Tomasz Szyperek <tomek.szyperek@gmail.com>
2023-04-03 18:08:16 +0200

    34

---
+++ src/main/java/pl/thekemotny/tasksthirtytothirtynine/taskrthirtyfour/Main.java
@@ -0,0 +1,10 @@
+package pl.thekemotny.tasksthirtytothirtynine.taskrthirtyfour;
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        MyThread myThread1 = new MyThread();
+        myThread.start();
+        myThread1.start();
+    }
+}
